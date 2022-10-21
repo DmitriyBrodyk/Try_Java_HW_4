5d78d2e909528cce10294a86133389275a0c4cfc
DmitriyBrodyk <brodyk.dmitriy@gmail.com>
2022-10-21 17:58:36 +0300

    Add 1 new class for Home Work 5.3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         Lecture Math = new Lecture(1134,"/ How use ++", 1,"Work Hard All Day", "Drink Energetics");
         Lecture English = new Lecture(223,"/ Past Simple", 2,"Work Hard All Day", "Drink Energetics");
         Lecture Other1 = new Lecture(311,"/ World Trands", 1,"Work Hard All Day", "Drink Energetics");
+
+        Lecture HomeWork_52_2 = new Lecture(1,"2", 1, "1","1");
         Lecture.printQ();
     }
 }
