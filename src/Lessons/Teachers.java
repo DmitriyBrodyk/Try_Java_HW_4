2b6109150a42b67dd4609f0d2a357c3ef1624eee
DmitriyBrodyk <brodyk.dmitriy@gmail.com>
2022-10-21 17:45:27 +0300

    Add all docs, before the work

--- src/Lessons/Teachers.java
+++ src/Lessons/Teachers.java
@@ -3,16 +3,12 @@ package Lessons;
 public class Teachers {
 
     private static int id;
-    private String firstName;
-    private String nameCourse;
 
-    public Teachers(int id, String firstName, String nameCourse) {
+    public Teachers(int id) {
         this.id = id;
-        this.firstName = firstName;
-        this.nameCourse = nameCourse;
 
     }
     public void printT(){
-        System.out.println("Id Teacher-"+ id+ " Name-"+ firstName + " Course#-" + nameCourse);
+        System.out.println("Id Teacher-"+ id);
     }
 }
--- src/Services/ServiceTeachers.java
+++ src/Services/ServiceTeachers.java
@@ -4,8 +4,8 @@ import Lessons.Teachers;
 
 public class ServiceTeachers {
     public static void main(String[] args) {
-        Teachers tch1 = new Teachers(1,"Lisa", " English");
-        Teachers tch2 = new Teachers(112, "Luise", " Math");
+        Teachers tch1 = new Teachers(1);
+        Teachers tch2 = new Teachers(112);
         tch1.printT();
         tch2.printT();
     }
